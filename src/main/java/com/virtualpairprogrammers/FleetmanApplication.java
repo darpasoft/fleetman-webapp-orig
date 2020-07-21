14d57e2d30c95c88e5d690998f730ffe8312c66c
darpasoft <igorssac@gmail.com>
2020-07-21 23:16:02 +0200

    Update FleetmanApplication.java

--- src/main/java/com/virtualpairprogrammers/FleetmanApplication.java
+++ src/main/java/com/virtualpairprogrammers/FleetmanApplication.java
@@ -7,6 +7,9 @@ import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 import com.netflix.ribbon.proxy.annotation.Hystrix;
 
