f71742e70d5677e0532f67de7bf63cf89cff1df4
darpasoft <igorssac@gmail.com>
2020-07-22 14:43:05 +0200

    Update FleetmanApplication.java

--- src/main/java/com/virtualpairprogrammers/FleetmanApplication.java
+++ src/main/java/com/virtualpairprogrammers/FleetmanApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 import com.netflix.ribbon.proxy.annotation.Hystrix;
 
